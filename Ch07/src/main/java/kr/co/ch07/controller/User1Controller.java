92205762986e5574b24d4a752d894935fd885f09
soungjun <jungy0309@naver.com>
2023-01-11 19:38:13 +0900

    2023/01/11 JPA 실습

---
+++ Ch07/src/main/java/kr/co/ch07/controller/User1Controller.java
@@ -0,0 +1,61 @@
+package kr.co.ch07.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.ch07.service.User1Service;
+import kr.co.ch07.vo.User1VO;
+
+@Controller
+@RequestMapping
+public class User1Controller {
+
+	@Autowired
+	private User1Service service;
+	
+	@GetMapping("/user1/list")
+	public String list(Model model) {
+		List<User1VO> users = service.selectUser1s(); 
+		model.addAttribute("users", users);
+		return "/user1/list";
+	}
+	
+	
+	@GetMapping("/user1/register")
+	public String register(Model model, User1VO user) {
+		model.addAttribute("user", user);
+		return "/user1/register";
+	}
+	
+	@PostMapping("/user1/register")
+	public String register(User1VO user) {
+		service.insertUser1(user);
+		return "redirect:/user1/list";
+	}
+	
+	
+	@GetMapping("/user1/modify")
+	public String modify(String uid, Model model) {
+		User1VO user = service.selectUser1(uid);
+		model.addAttribute("user", user);
+		return "/user1/modify";
+	}
+	
+	@PostMapping("/user1/modify")
+	public String modify(User1VO user) {
+		service.updateUser1(user);
+		return "redirect:/user1/list";
+	}
+		
+	@GetMapping("/user1/delete")
+	public String delete(String uid) {
+		service.deleteUser1(uid);
+		return "redirect:/user1/list";
+	}
+}
