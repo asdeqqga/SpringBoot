382f59c45f87c4c435029cc5a02f81f3a967cc99
soungjun <jungy0309@naver.com>
2023-01-16 19:19:21 +0900

    2023/01/16 수정

--- Ch07/src/main/java/kr/co/ch07/service/User2Service.java
+++ Ch07/src/main/java/kr/co/ch07/service/User2Service.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.ch07.dao.User2DAO;
 import kr.co.ch07.repository.User2Repo;
 import kr.co.ch07.vo.User2VO;
 
@@ -13,50 +12,23 @@ import kr.co.ch07.vo.User2VO;
 public class User2Service {
 	
 	@Autowired
-	private User2DAO dao;
-	
-	@Autowired
 	private User2Repo repo;
 
 	public void insertUser2(User2VO vo) {
-		// Mybatis
-		//dao.insertUser1(vo);
-		
-		// JPA
 		repo.save(vo);		
 	}
-	
 	public User2VO selectUser2(String uid) {
-		// Mybatis
-		//User1VO user = dao.selectUser1(uid);
-		
-		// JPA
 		User2VO user = repo.findById(uid).get();
-		
 		return user;
 	}
-	
 	public List<User2VO> selectUser2s() {
-		// Mybatis
-		//List<User1VO> users = dao.selectUser1s();
-		
-		// JPA
 		List<User2VO> users = repo.findAll();
-		
 		return users;
 	}
 	public void updateUser2(User2VO vo) {
-		// Mybatis
-		//dao.updateUser1(vo);
-		
-		// JPA
 		repo.save(vo);
 	}
 	public void deleteUser2(String uid) {
-		// Mybatis
-		//dao.deleteUser1(uid);
-		
-		// JPA
 		repo.deleteById(uid);
 	}
 }
--- Ch07/src/main/java/kr/co/ch07/service/User3Service.java
+++ Ch07/src/main/java/kr/co/ch07/service/User3Service.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.ch07.dao.User3DAO;
 import kr.co.ch07.repository.User3Repo;
 import kr.co.ch07.vo.User3VO;
 
@@ -13,50 +12,23 @@ import kr.co.ch07.vo.User3VO;
 public class User3Service {
 	
 	@Autowired
-	private User3DAO dao;
-	
-	@Autowired
 	private User3Repo repo;
 
 	public void insertUser3(User3VO vo) {
-		// Mybatis
-		//dao.insertUser1(vo);
-		
-		// JPA
 		repo.save(vo);		
 	}
-	
 	public User3VO selectUser3(String uid) {
-		// Mybatis
-		//User1VO user = dao.selectUser1(uid);
-		
-		// JPA
 		User3VO user = repo.findById(uid).get();
-		
 		return user;
 	}
-	
 	public List<User3VO> selectUser3s() {
-		// Mybatis
-		//List<User1VO> users = dao.selectUser1s();
-		
-		// JPA
 		List<User3VO> users = repo.findAll();
-		
 		return users;
 	}
 	public void updateUser3(User3VO vo) {
-		// Mybatis
-		//dao.updateUser1(vo);
-		
-		// JPA
 		repo.save(vo);
 	}
 	public void deleteUser3(String uid) {
-		// Mybatis
-		//dao.deleteUser1(uid);
-		
-		// JPA
 		repo.deleteById(uid);
 	}
 }
