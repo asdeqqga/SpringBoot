37befa677fd9edbeacd72768bf3a8495b3cfe9ce
soungjun <jungy0309@naver.com>
2023-01-18 18:48:13 +0900

    2023/01/18 Spirng 게시판 실습

--- Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
+++ Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -1,13 +1,25 @@
 package kr.co.sboard.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.co.sboard.entity.UserEntity;
+import kr.co.sboard.security.MyUserDetails;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class ArticleController {
 	
 	@GetMapping("list")
-	public String list() {
+	public String list(@AuthenticationPrincipal MyUserDetails myUser, Model model) {
+		
+		UserEntity user = myUser.getUser();
+		model.addAttribute("user", user);
 		return "list";
 	}
 	
