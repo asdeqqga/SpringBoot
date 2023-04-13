80cff881744f90066e5c599d8765ff918e6316b1
soungjun <jungy0309@naver.com>
2023-04-13 18:27:11 +0900

    2023/01/16 Spring REST 웹 서비스 실습

--- Ch09/src/main/java/kr/co/ch09/vo/User1VO.java
+++ Ch09/src/main/java/kr/co/ch09/vo/User1VO.java
@@ -5,14 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ToString
 public class User1VO {
-	
 	private String uid;
 	private String pass;
 	private String name;
